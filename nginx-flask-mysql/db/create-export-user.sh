843dc85cd861dac053d55106cfecf48f2430e853
Shekhawat123 <165200537+Shekhawat123@users.noreply.github.com>
2025-08-13 00:56:32 +0530

    Update create-export-user.sh
    Signed-off-by: Shekhawat123 <165200537+Shekhawat123@users.noreply.github.com>

--- nginx-flask-mysql/db/create-export-user.sh
+++ nginx-flask-mysql/db/create-export-user.sh
@@ -4,12 +4,6 @@ set -e
 # Read the password from the secret file
 export MYSQL_ROOT_PASSWORD="$(cat /run/secrets/db-password)"
 
-# Wait until DB is healthy
-echo "Waiting for DB to become healthy..."
-until docker inspect --format='{{json .State.Health.Status}}' nginx-flask-mysql-db-1 | grep -q healthy; do
-  sleep 2
-done
-
 echo "DB is healthy. Creating exporter user..."
 mysql -h db -u root -p"$(cat /run/secrets/db-password)" -e "
 CREATE USER IF NOT EXISTS 'exporter'@'%' IDENTIFIED BY 'exporterpass';
