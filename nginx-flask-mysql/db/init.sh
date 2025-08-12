06978c52820f1caf6a61c87ffe8c4b10ac2f6c2d
Shekhawat123 <165200537+Shekhawat123@users.noreply.github.com>
2025-08-12 23:47:51 +0530

    Create init.sh
    wrapper script to create user that will export the data to prometheus
    
    Signed-off-by: Shekhawat123 <165200537+Shekhawat123@users.noreply.github.com>

---
+++ nginx-flask-mysql/db/init.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+# Start MySQL in the background
+mysqld &
+
+# Wait for MySQL to be ready
+until mysqladmin ping --silent; do
+  sleep 1
+done
+
+# Create exporter user if not exists
+mysql -u root -p"$(cat /db/password.txt)" <<EOF
+CREATE USER IF NOT EXISTS 'exporter'@'%' IDENTIFIED BY 'exporterpass';
+GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+# Bring MySQL to foreground
+wait
