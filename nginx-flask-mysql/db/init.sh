ace12a1409762ef1e19a56efc9c0ac62c02eefdf
Shekhawat123 <165200537+Shekhawat123@users.noreply.github.com>
2025-08-13 00:08:20 +0530

    Update init.sh
    Signed-off-by: Shekhawat123 <165200537+Shekhawat123@users.noreply.github.com>

--- nginx-flask-mysql/db/init.sh
+++ nginx-flask-mysql/db/init.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
-set -e
 
-# Start MySQL in the background
-mysqld &
-
-# Wait for MySQL to be ready
-until mysqladmin ping --silent; do
-  sleep 1
-done
-
-# Create exporter user if not exists
+# Run your SQL setup
 mysql -u root -p"$(cat /run/secrets/db-password)" <<EOF
 CREATE USER IF NOT EXISTS 'exporter'@'%' IDENTIFIED BY 'exporterpass';
 GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'%';
@@ -17,5 +8,5 @@ FLUSH PRIVILEGES;
 EOF
 
 # Start MariaDB
-exec mysqld
+exec mysqld --default-authentication-plugin=mysql_native_password
 
