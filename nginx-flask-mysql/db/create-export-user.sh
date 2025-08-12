d10c0aa99e3f3ad3383d7c444a807b20cf88ee96
Shekhawat123 <165200537+Shekhawat123@users.noreply.github.com>
2025-08-13 01:04:56 +0530

    Update create-export-user.sh
    Signed-off-by: Shekhawat123 <165200537+Shekhawat123@users.noreply.github.com>

--- nginx-flask-mysql/db/create-export-user.sh
+++ nginx-flask-mysql/db/create-export-user.sh
@@ -4,7 +4,7 @@ set -e
 export MYSQL_ROOT_PASSWORD="$(cat /run/secrets/db-password)"
 
 echo "Creating exporter user..."
-mariadb -h db -u root -p"$MYSQL_ROOT_PASSWORD" <<EOF
+mariadb --ssl-mode=DISABLED -h db -u root -p"$MYSQL_ROOT_PASSWORD" <<EOF
 CREATE USER IF NOT EXISTS 'exporter'@'%' IDENTIFIED BY 'exporterpass';
 GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'exporter'@'%';
 FLUSH PRIVILEGES;
